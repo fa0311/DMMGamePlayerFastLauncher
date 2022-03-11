1bfa4158d1ee0677acd4fdb18df80e98343b6b83
fa0311 <yukinashi2742756@yahoo.co.jp>
2022-03-12 04:29:27 +0900

    update black reformat

--- DMMGamePlayerFastLauncher.py
+++ DMMGamePlayerFastLauncher.py
@@ -14,7 +14,11 @@ argpar = argparse.ArgumentParser(
 
 argpar.add_argument("product_id")
 argpar.add_argument("game_path")
-argpar.add_argument("-dgp-path", "--dmmgameplayer-path", default="C:/Program Files/DMMGamePlayer/DMMGamePlayer.exe")
+argpar.add_argument(
+    "-dgp-path",
+    "--dmmgameplayer-path",
+    default="C:/Program Files/DMMGamePlayer/DMMGamePlayer.exe",
+)
 argpar.add_argument("--kill", default=True)
 arg = argpar.parse_args()
 
@@ -34,24 +38,33 @@ params = {
     "mac_address": "00:11:22:33:44:55",
     "hdd_serial": "0000000011111111222222223333333344444444555555556666666677777777",
     "motherboard": "0000000011111111222222223333333344444444555555556666666677777777",
-    "user_os": "win"
+    "user_os": "win",
 }
 
+
 def get_dmm_cookie():
-    process = subprocess.Popen(args="", executable=arg.dmmgameplayer_path, shell=True, stdout=subprocess.PIPE)
+    process = subprocess.Popen(
+        args="", executable=arg.dmmgameplayer_path, shell=True, stdout=subprocess.PIPE
+    )
     for line in process.stdout:
-        text = (line.decode("utf8").strip())
+        text = line.decode("utf8").strip()
         if 'Header key: "cookie"' in text:
             if arg.kill:
                 process.terminate()
             return re.findall(r'"(.*?)"', text)[1]
 
+
 dmm_cookie = get_dmm_cookie()
 if dmm_cookie == None:
-    raise Exception('DMMGamePlayerが起動できませんでした\nDMMGamePlayerがすでに起動されている可能性があります')
+    raise Exception("DMMGamePlayerが起動できませんでした\nDMMGamePlayerがすでに起動されている可能性があります")
 
 headers["cookie"] = dmm_cookie
-response = requests.session().post("https://apidgp-gameplayer.games.dmm.com/v5/launch/cl", headers=headers, json=params,verify=False)
+response = requests.session().post(
+    "https://apidgp-gameplayer.games.dmm.com/v5/launch/cl",
+    headers=headers,
+    json=params,
+    verify=False,
+)
 dmm_args = response.json()["data"]["execute_args"].split(" ")
 
 subprocess.Popen([arg.game_path, dmm_args[0], dmm_args[1]])
