8dad0691ded6ecdf21edb97898cf2b12cb249369
fa0311 <yukinashi2742756@yahoo.co.jp>
2022-03-18 18:18:16 +0900

    add anonymous mode

--- DMMGamePlayerFastLauncher.py
+++ DMMGamePlayerFastLauncher.py
@@ -5,6 +5,8 @@ import re
 import json
 import glob
 import win32crypt
+import random
+import hashlib
 
 requests.packages.urllib3.disable_warnings()
 
@@ -24,27 +26,20 @@ argpar.add_argument(
 argpar.add_argument("--non-kill", action="store_true")
 argpar.add_argument("--debug", action="store_true")
 argpar.add_argument("--login-force", action="store_true")
+argpar.add_argument("--anonymous", action="store_true")
 arg = argpar.parse_args()
 
-headers = {
-    "Host": "apidgp-gameplayer.games.dmm.com",
-    "Connection": "keep-alive",
-    "User-Agent": "DMMGamePlayer5-Win/17.1.2 Electron/17.1.2",
-    "Client-App": "DMMGamePlayer5",
-    "Client-version": "17.1.2",
-}
+def gen_rand_hex():
+    return hashlib.sha256(str(random.random()).encode()).hexdigest()
 
-params = {
-    "product_id": arg.product_id,
-    "game_type": "GCL",
-    "game_os": "win",
-    "launch_type": "LIB",
-    "mac_address": "00:11:22:33:44:55",
-    "hdd_serial": "0000000011111111222222223333333344444444555555556666666677777777",
-    "motherboard": "0000000011111111222222223333333344444444555555556666666677777777",
-    "user_os": "win",
-}
-
+def gen_rand_address():
+    hex = gen_rand_hex()
+    address = ""
+    for x in range(12):
+        address += hex[x]
+        if x % 2 == 1:
+            address += ":"
+    return address[:-1]
 
 class dgp5_process:
     def __init__(self):
@@ -66,6 +61,16 @@ class dgp5_process:
                 return install_data
         self._end_stdout()
 
+    def get_client_data(self):
+        for line in self.process.stdout:
+            text = self._decode(line)
+            if (
+                "https://apidgp-gameplayer.games.dmm.com/v5/gameplayer/agreement/check"
+                in text
+            ):
+                return self._req_body_to_dict(text)
+        self._end_stdout()
+
     def get_cookie(self):
         for line in self.process.stdout:
             text = self._decode(line)
@@ -81,6 +86,9 @@ class dgp5_process:
     def kill(self):
         self.process.terminate()
 
+    def _req_body_to_dict(self, line):
+        return json.loads(line.split(" :: Request body is ")[1][:-1])
+
     def _decode(self, line):
         text = line.decode("utf-8").strip()
         if self.debug:
@@ -91,6 +99,31 @@ class dgp5_process:
         raise Exception("DMMGamePlayerの実行中にエラーが発生しました\n既に実行されているか実行中に終了した可能性があります")
 
 
+headers = {
+    "Host": "apidgp-gameplayer.games.dmm.com",
+    "Connection": "keep-alive",
+    "User-Agent": "DMMGamePlayer5-Win/17.1.2 Electron/17.1.2",
+    "Client-App": "DMMGamePlayer5",
+    "Client-version": "17.1.2",
+}
+
+params = {
+    "product_id": arg.product_id,
+    "game_type": "GCL",
+    "game_os": "win",
+    "launch_type": "LIB",
+}
+
+if arg.anonymous:
+    client_data = {
+        "mac_address": gen_rand_address(),
+        "hdd_serial": gen_rand_hex(),
+        "motherboard": gen_rand_hex(),
+        "user_os": "win",
+    }
+
+
+
 open("cookie.bytes", "a+")
 with open("cookie.bytes", "rb") as f:
     blob = f.read()
@@ -99,6 +132,9 @@ process = dgp5_process()
 process.debug = arg.debug
 install_data = process.get_install_data()
 
+if not arg.anonymous:
+    client_data = process.get_client_data()[0]
+
 if blob == b"" or arg.login_force:
     cookie = process.get_cookie()
     new_blob = win32crypt.CryptProtectData(cookie.encode(), "DMMGamePlayerFastLauncher")
@@ -106,8 +142,6 @@ if blob == b"" or arg.login_force:
         f.write(new_blob)
 else:
     _, cookie = win32crypt.CryptUnprotectData(blob)
-
-headers["cookie"] = cookie
 
 if not arg.game_path:
     for contents in install_data["contents"]:
@@ -137,6 +171,11 @@ if not arg.game_path:
             + " ".join([contents["productId"] for contents in install_data["contents"]])
             + "から選択して下さい"
         )
+
+headers["cookie"] = cookie
+params.update(client_data)
+print(params)
+
 response = requests.post(
     "https://apidgp-gameplayer.games.dmm.com/v5/launch/cl",
     headers=headers,
