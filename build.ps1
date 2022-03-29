968727a735e6a49a50ec0800ad17ee61a19f6dd6
fa0311 <yukinashi2742756@yahoo.co.jp>
2022-03-29 19:34:17 +0900

    update build.ps1

--- build.ps1
+++ build.ps1
@@ -2,4 +2,5 @@ black *.py
 pip freeze > requirements.txt
 pyinstaller DMMGamePlayerFastLauncher.py --onefile --noconsole
 pyinstaller DMMGamePlayerProductIdChecker.py --onefile
+Copy-Item -Path "C:\Project\Python\hack\DMMGamePlayerFastLauncher\dist\DMMGamePlayerProductIdChecker.exe" -Destination "C:\Project\Python\hack\DMMGamePlayerFastLauncher\windows\tools" -Force
 start "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" "C:\Project\Python\hack\DMMGamePlayerFastLauncher\setup.iss"
