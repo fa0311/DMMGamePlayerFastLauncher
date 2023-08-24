3662758498bdad23013e949605c4b5a85e9afa5c
ふぁ <yuki@yuki0311.com>
2023-08-24 20:00:12 +0900

    update build
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- tools/build.ps1
+++ tools/build.ps1
@@ -5,12 +5,7 @@ python .\tools\build.py
 
 pyinstaller DMMGamePlayerFastLauncher\DMMGamePlayerFastLauncher.py --noconsole --onefile --add-data ".venv\Lib\site-packages\customtkinter\;customtkinter" --icon assets\icons\DMMGamePlayerFastLauncher.ico
 
-# pyinstaller DMMGamePlayerProductIdChecker.py --onefile
-# pyinstaller Task.py --onefile --noconsole
-New-Item "windows\tools" -ItemType Directory -Force
-
-# Copy-Item -Path "dist\DMMGamePlayerProductIdChecker.exe" -Destination "windows\tools" -Force
-# Copy-Item -Path "dist\Task.exe" -Destination "windows\tools" -Force
+Copy-Item -Path "dist\DMMGamePlayerFastLauncher.exe" -Destination "windows" -Force
 
 
 Copy-Item -Path "assets" -Destination "windows" -Force -Recurse
