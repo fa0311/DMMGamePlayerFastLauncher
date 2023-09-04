c9653d042c57caf2d092977f69b38fb4f60bcd0a
ふぁ <yuki@yuki0311.com>
2023-09-04 09:52:44 +0900

    update version
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- DMMGamePlayerFastLauncher/static/env.py
+++ DMMGamePlayerFastLauncher/static/env.py
@@ -14,7 +14,7 @@ class Dump:
 
 
 class Env(Dump):
-    VERSION = "v5.0.1"
+    VERSION = "v5.0.2"
     RELEASE_VERSION = requests.get(UrlConfig.RELEASE_API).json().get("tag_name", VERSION)
 
     DEVELOP: bool = os.environ.get("ENV") == "DEVELOP"
