a6552b70636a46f276db933eb6a71c0aa6147403
ふぁ <yuki@yuki0311.com>
2024-02-20 17:34:37 +0900

    update 5.5.0
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- DMMGamePlayerFastLauncher/static/env.py
+++ DMMGamePlayerFastLauncher/static/env.py
@@ -8,7 +8,7 @@ from windows_pathlib import WindowsPathlib
 
 
 class Env(Dump):
-    VERSION = "v5.4.1"
+    VERSION = "v5.5.0"
     RELEASE_VERSION = requests.get(UrlConfig.RELEASE_API).json().get("tag_name", VERSION)
 
     DEVELOP: bool = os.environ.get("ENV") == "DEVELOP"
