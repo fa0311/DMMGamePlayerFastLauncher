97958dd9636d6db2e7c4d851ff6e39b9f96febb5
ふぁ <yuki@yuki0311.com>
2024-02-22 05:55:18 +0900

    add Path existence check
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- DMMGamePlayerFastLauncher/static/loder.py
+++ DMMGamePlayerFastLauncher/static/loder.py
@@ -10,6 +10,11 @@ from utils.utils import get_supported_lang
 
 
 def config_loder():
+    if not DataPathConfig.DATA.exists():
+        raise FileNotFoundError(f"{DataPathConfig.DATA} not found")
+    if not AssetsPathConfig.PATH.exists():
+        raise FileNotFoundError(f"{AssetsPathConfig.PATH} not found")
+
     if DataPathConfig.APP_CONFIG.exists():
         with open(DataPathConfig.APP_CONFIG, "r", encoding="utf-8") as f:
             AppConfig.DATA = SettingData.from_dict(json.load(f))
